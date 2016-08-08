9be50be36c21276b20179b3cad0d1e610d1a8609
joc-kmilo <camilo_1574@hotmail.com>
2016-08-08 13:55:39 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,248 @@
-<?php
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Transporte y reciclaje de alimentos</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" />
 
-require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+		<script type="text/javascript" src="lib/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="lib/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="lib/jquery.coda-slider-2.0.js"></script>
+<!-- Initialize each slider on the page. Each slider must have a unique id -->
+	<script type="text/javascript">
+	$().ready(function() {
+	$('#coda-slider-2').codaSlider({
+		autoSlide: true,
+		autoSlideInterval: 6000,
+		autoSlideStopWhenClicked: true
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+	});
+ });
+</script>
+<!-- End JavaScript -->
 
-// Our web handlers
+<!-- Pirobox setup and styles -->
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+<script type="text/javascript" src="lib/pirobox.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$().piroBox({
+			my_speed: 400, //animation speed
+			bg_alpha: 0.1, //background opacity
+			slideShow : false, // true == slideshow on, false == slideshow off
+			slideSpeed : 20, //slideshow duration in seconds(3 to 6 Recommended)
+			close_all : '.piro_close,.piro_overlay'// add class .piro_overlay(with comma)if you want overlay click close piroBox
+
+	});
 });
+</script>
 
-$app->run();
+<link href="images/style.css" rel="stylesheet" type="text/css" />
+
+<!-- Pirobox setup and styles end-->
+
+</head>
+<body>
+
+<div id="main_bg">
+<div id="main">
+<!-- header -->
+<div id="header">
+	<div id="logo">
+		<h1 align=light> <font size=10 color=#ffffff> Problem&aacute;tica Ambiental </font> </h1>
+	</div>
+ <div id="buttons">
+	<a href="index.html" class="but" title="">Inicio</a><div class="but_razd"></div>
+	<a href="blog.html" class="but" title="">Estad&iacute;sticas</a><div class="but_razd"></div>
+	<a href="gallery.html" class="but" title="">Galer&iacute;a</a><div class="but_razd"></div>
+	<a href="about_us.html" class="but" title="">About us</a><div class="but_razd"></div>
+	<a href="contact_us.html" class="but" title="">Cont&aacute;cto</a>
+	</div>
+</div><div class="inner_copy"></div>
+<!-- / header -->
+<div class="top">
+<div class="top_text">
+
+		<div class="coda-slider-wrapper">
+	<div class="coda-slider preload" id="coda-slider-2">
+		<div class="panel">
+			<div class="panel-wrapper">
+				<div class="my_header"><img src="images/sabila.jpg" width="430" height="278" class="top_img" alt="" style="text-align: justify;" />
+					<h1>Aloe vera</h1>
+					<body style="text-align: justify;">Tambien conocida como s&aacute;bila, es una planta suculenta de la subfamilia Asphodeloideae dentro de la familia Xanthorrhoeaceae.
+					El Aloe vera es un producto ampliamente usado en alimentaci&oacute;n y cosm&eacute;tica. Aunque es considerado una planta medicinal por sus m&uacute;ltiples 
+					propiedades, no es un producto inocuo, ya que presenta ciertos componentes irritantes que pueden producir efectos secundarios.
+					</body>
+					<div class="read_l"><a href="gallery.html">Leer m&aacute;s</a></div>
+					</div>
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+
+				<div class="my_header">
+						<div class="scroll_left"><img src="images/mango.jpg" class="top_img" alt="" /></div>
+						<div class="scroll_right">
+							<div class="scroll_left_inside"><h1>Mango</h1>
+							<body style="text-align: justify;">
+							Es una especie arb&oacute;rea perteneciente a la familia de las anacardi&aacute;ceas. 
+							El mango es una fruta de la Zona Intertropical de pulpa carnosa y dulce. Destaca entre sus principales caracter&iacute;sticas su buen sabor.
+							</body>
+							</div>
+							<div class="scroll_right_inside">
+							<h1>Tipos</h1>
+							<ul>
+								<li><a href="#">Haitian</a></li>
+								<li><a href="#">Tommy Atkins</a></li>
+								<li><a href="#">Haden</a></li>
+								<li><a href="#">Ataulfo</a></li>
+								<li><a href="#">Kent</a></li>
+								<li><a href="#">Manila</a></li>
+								<li><a href="#">Keitt</a></li>
+							</ul>
+							</div>
+						</div>
+					</div>
+
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+				<div class="my_header"><img src="images/limon.jpg" class="top_img" alt="" />
+					<h1>Lim&oacute;n</h1>
+					<body style="text-align: justify;">
+					Es originario de Asia, una fruta comestible de sabor &aacute;cido y extremadamente fragante que se usa 
+					principalmente en la alimentaci&oacute;n. Su fruto posee un alto contenido en vitamina C (501,6 mg/L) y &aacute;cido c&iacute;trico (49,88 g/L).
+					El lim&oacute;n es utilizado en multitud de comidas y bebidas, a veces para dar sabor o solo un toque de acidez a un bistec, pescado o guiso. 
+					Pero adem&aacute;s de su rico sabor en tartas y helados de lim&oacute;n est&aacute;n sus grandiosas propiedades curativas. Su gran aporte de vitaminas le 
+					convierte en un gran depurador de toxinas y un implacable bactericida.
+					</body>
+					<div class="read_l"><a href="gallery.html">Leer m&aacute;s</a></div>
+					</div>
+
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+
+				<div class="my_header">
+					<div class="scroll_left"><img src="images/papaya.jpg" class="top_img" alt="" /></div>
+					<div class="scroll_right">
+						<p><a href="">Papaya</a><br/>
+						 Es una especie de planta de la familia de las caric&aacute;ceas. Es una fruta destacada por contener una gran cantidad de vitaminas
+						 (entre las que encontramos vitaminas del grupo B, C, A y D), minerales (como el calcio, el potasio o el sodio), y fibra diet&eacute;tica.
+						</p>
+				<div class="read_l"><a href="gallery.html">Leer m&aacute;s</a></div>
+						<p><a href="">Tipos</a><br/>
+						Los arbustos de papayo tienen tres clases de pies diferentes; unos con flores femeninas, otros con flores hermafroditas y otros con flores masculinas.
+						</p>
+		<div class="read_l"><a href="gallery.html">Leer m&aacute;s</a></div>
+
+					</div>
+					</div>
+
+			</div>
+		</div>
+	</div><!-- .coda-slider -->
+</div>
+
+
+	</div>
+</div>
+
+<!-- content -->
+				<div class="cont_top"></div>
+				<div id="content">
+					<div id="cont_razd">
+					<div id="right">
+						<h1>Latest News</h1>
+						<span class="span_dat">Apr. 11, 2010</span><img src="images/img14.jpg" class="img_l" alt="" /><br/>
+						<span class="span_cont">Suspendisse rutrum interdum lacinia.</span><div style="height:5px"></div>
+						Suspendisse tempus aliquet elit sit amet pellentesque. Donec iaculis pulvinar mauris, ac vulputate justo pretium quis.
+						<div class="read_l"><a href="#">read more</a></div>
+						<span class="span_dat">Apr. 10, 2010</span><br/>
+						<span class="span_cont">Duis a molestie massa.</span><div style="height:5px"></div>
+						Nullam imperdiet vulputate volutpat. Nunc consectetur mattis accumsan. Etiam
+						<div class="read_l"><a href="#">read more</a></div>
+					</div>
+			<div id="left">
+								<h1>Bienvenido a nuestro sitio web</h1>
+							<div class="text">
+								<img src="images/comida.jpg" class="img_l" width="200" height="133" alt="" /> <span class="span_cont"> El desperdicio de comida </span><br/>
+								Actualmente se desperdician 1'600.000 toneladas de comida al año en el pais, una cifra increiblemente alta. <br/>
+								<img src="images/fish1.gif" alt="" />Mauris massa diam, ornare vel ultrices eget, vulputate dictum dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam ut mauris sed nibh porta vehicula quis eget dolor. <br/>
+								<img src="images/fish1.gif" alt="" />Phasellus sed ligula nisl. Proin et risus vel ligula vehicula pharetra. Donec vestibu- lum posuere erat, quis porta eros ultrices quis. Nullam fringilla dolor sit amet nulla. <br/>
+								Nullam fringilla dolor sit amet nulla dictum porttitor a at orci. Fusce at tortor est. Morbi eget leo vitae nibh aliquam elementum in id mauris. Fusce in elit nec lectus mollis bibendum. Integer eu lectus at tortor vestibulum rutrum. Pellentesque rutrum, ipsum eu adipiscing ornare, elit erat dignissim mi, vitae feugiat orci mi a dolor. Maecenas sapien turpis, faucibus ac mattis.<br/><div style="height:13px"></div>
+								<img src="images/perdida.jpg" class="img_r" width="160" height="102" alt="" />
+								<span class="span_cont">Cras volutpat dui sit amet eros bibendum volutpat. </span><br/>
+									Curabitur eu lectus mi, in volutpat eros. Morbi ornare mattis libero, vulputate vulputate velit sollicitudin vel. In auctor mattis nisi et malesuada. Proin sed mi sapien. Nulla accumsan mattis elementum. Pellentesque luctus enim non eros pellentesque molestie. Mauris vulputate aliquam mattis. Donec varius, tortor sed aliquam accumsan, dolor est suscipit ante, quis molestie quam sapien a dolor. Morbi id lobortis urna. Aliquam at porta mi. Aenean varius.<br/>
+								<div class="next"><a href="#">Next </a><img src="images/fish_next.gif" alt="" /></div>
+							</div>
+
+						</div>
+						<div style="clear: both"></div>
+					</div>
+				</div>
+				<div class="cont_bot"></div>
+<!-- / content -->
+	<div style="height:15px"></div>
+<!-- bottom -->
+	<div id="bottom">
+		<div id="b_col1">
+		<h1>Recent Posts</h1>
+			<ul>
+									<li><a href="#">Free Website Templates</a></li>
+									<li><a href="#">Free Flash Templates</a></li>
+									<li><a href="#">Easy Flash Templates</a></li>
+		<li><a href="#">Top Hosting Providers</a></li>
+									<li><a href="#">Free Fonts</a></li>
+									<li><a href="#">Free Icons</a></li>
+									<li><a href="#">Free Photoshop Brushes</a></li>
+									<li><a href="#">RSS Subscribe</a></li>
+			</ul>
+		</div>
+		<div id="b_col2">
+			<h1>Follow Us</h1>
+			<ul>
+				<li><img src="images/fu_i1.gif" class=" fu_i" alt="" /><a href="#">Lorem ipsum dolor</a></li>
+				<li><img src="images/fu_i2.gif" class=" fu_i" alt="" /><a href="#">Maecenas in turpis </a></li>
+				<li><img src="images/fu_i3.gif" class=" fu_i" alt="" /><a href="#">Morbi fringilla libero</a></li>
+				<li><img src="images/fu_i4.gif" class=" fu_i" alt="" /><a href="#">In venenatis metus vel </a></li>
+			</ul>
+
+	</div>
+		<div id="b_col3">
+			<h2>Latest Projects</h2>
+			<a href="images/project1.jpg" class="pirobox_footer" title="First Project Screenshot"><img src="images/bottom_i1.jpg" alt="" /></a><div style="height:10px"></div>
+			<a href="images/project2.jpg" class="pirobox_footer" title="Second Project Screenshot"><img src="images/bottom_i2.jpg" alt="" /></a>
+		</div>
+	<div id="b_col4">
+			<h2>Send a Message</h2>
+			<form action="" method="post">
+					<div><input class="input_txt" value="Name" name="Name" type="text" /></div><div style="height:5px"></div>
+					<div> <input class="input_txt" value="E-mail" name="E-mail" type="text" /></div><div style="height:5px"></div>
+					<div><input class="input_txt" value="Subject" name="Subject" type="text" /></div><div style="height:5px"></div>
+					<div><textarea class="text_area" cols="32" rows="3" name="Message">Message</textarea></div>
+				<div><input class="submit" name="Submit" type="submit" value="Submit" /></div>
+		</form>
+	</div>
+	<div style="clear: both"></div>
+	</div>
+
+<!-- / bottom -->
+<!-- footer -->
+	<div id="footer">
+		<p>Copyright statement. Designed by <a href="http://www.metamorphozis.com">Flash Templates</a></p>
+		<p><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+		<p>Busque m&aacute;s plantillas web gratis <a href="http://www.mejoresplantillasgratis.es" target="_blank">en MPG.es</a>.</p>
+	</div>
+<!-- / footer -->
+</div>
+
+</div>
+
+</body>
+</html>
